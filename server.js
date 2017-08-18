4e2db8a65ffca1c0e081070928866884c9b96305
ramkeelai <30689288+ramkeelai@users.noreply.github.com>
2017-08-18 22:39:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = require('crypto');
 var app = express();
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
